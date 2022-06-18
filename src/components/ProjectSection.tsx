cb0367e0e9dbbd3ea3c048d50c1508cff75ae585
alex <alexlhliang@gmail.com>
2022-06-18 14:35:39 -0700

    added favicon

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -7,7 +7,6 @@ import styles from '../styles/ProjectSection.module.scss';
 import crochessScreen from '../assets/images/crochess-screen.png';
 import breadScreen from '../assets/images/discord-screen.png';
 import wheresWaldoScreen from '../assets/images/wheres_waldo_screen.png';
-import battleshipScreen from '../assets/images/battleship_screen.png';
 import battleshipScreen3 from '../assets/images/battleship_screen_3.png';
 import memoryCardScreen from '../assets/images/memory_card_game_screen.png';
 
