c36bc66796b177c9475bfabba8b0a99bcf7417f6
alex <alexlhliang@gmail.com>
2022-06-27 11:08:58 -0700

    finished cloud animation

--- src/components/CloudSvg.tsx
+++ src/components/CloudSvg.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { animated } from 'react-spring';
+import React, { RefObject } from 'react';
+import { animated, SpringValue, SpringRef } from 'react-spring';
 
 interface CloudSvgProps {
   className?: string;
+  style?: {
+    transform: SpringValue<string>;
+  };
+  listRef?:
+    | ((instance: SVGSVGElement | null) => void)
+    | RefObject<SVGSVGElement>
+    | null
+    | undefined;
 }
 
-export default function CloudSvg({ className }: CloudSvgProps) {
+export default function CloudSvg({
+  className,
+  style,
+  listRef
+}: CloudSvgProps) {
   const rootClasses = [];
   if (className) rootClasses.push(className);
   return (
-    <svg
+    <animated.svg
+      ref={listRef}
       className={rootClasses.join(' ')}
+      style={style}
       width="300px"
       height="300px"
       version="1.1"
@@ -23,6 +37,6 @@ export default function CloudSvg({ className }: CloudSvgProps) {
         d="m234.38 274.3c0.32813-0.49219 0.57422-1.0664 0.82031-1.6406-1.8047-4.7578-3.1992-9.6797-4.1836-14.684-1.1484-5.8242-1.7227-11.812-1.7227-17.719 0-24.691 9.8438-48.066 27.398-65.375 17.719-17.473 41.422-27.07 66.277-27.07 21.41 0 41.996 7.1367 58.73 20.426 16.488 13.125 28.629 31.418 32.812 52.25v0.082031c10.008-10.418 23.461-15.75 37.898-14.355 8.0391 0.82031 15.832 3.6094 22.559 8.0391 6.7266 4.3477 12.387 10.336 16.16 17.391 6.6445 12.305 7.1367 26.25 1.2305 38.965 0.082031 0.082031 0.082031 0.16406 0.16406 0.24609 18.211 1.0664 35.273 8.8594 47.902 21.984 12.551 13.125 19.605 30.434 19.605 48.559 0 18.867-7.5469 36.75-21 50.035-13.535 13.371-31.664 20.672-50.691 20.672h-276.6c-19.031 0-37.16-7.3828-50.691-20.672-13.453-13.207-21-31.09-21-50.035 0-18.867 7.5469-36.75 21-50.035 13.535-13.371 31.664-20.672 50.691-20.672 4.1836 0 8.3672 0.32812 12.469 1.0664 1.0664 0.16406 2.1328 0.32813 3.1992 0.57422 2.4609 0.41016 4.7578 1.1484 6.9727 1.9688z"
         fillRule="evenodd"
       />
-    </svg>
+    </animated.svg>
   );
 }
--- src/components/HeaderBg.tsx
+++ src/components/HeaderBg.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useLayoutEffect } from 'react';
 import { useSprings, animated } from 'react-spring';
 import CloudSvg from './CloudSvg';
 import styles from '../styles/HeaderBg.module.scss';
@@ -10,24 +10,51 @@ interface HeaderBgProps {
   };
 }
 
+function getRdmNum(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 export default function HeaderBg({ size }: HeaderBgProps) {
   const node = useRef<HTMLDivElement | null>(null);
+  const initialClouds = useRef<SVGElement[]>([]);
 
-  const [springs, api] = useSprings(12, (index) => ({ left: '100vw' }));
+  const [springs, api] = useSprings(8, (index: number) => {
+    const start = getRdmNum(100 - index * 2); // index*2 so clouds are more spread out
+    const end = start + 100;
 
-  useEffect(() => {
+    return {
+      from: {
+        transform: `translateX(${start}vw)`
+      },
+      to: {
+        transform: `translateX(${end}vw)`
+      },
+      config: {
+        duration: 70000
+      }
+    };
+  });
+
+  useLayoutEffect(() => {
     api.start();
   }, []);
 
   const rootClasses = ['bg', styles.main];
   return (
     <div ref={node} className={rootClasses.join(' ')}>
-      {springs.map((anime, i) => (
-        <animated.div style={anime} key={i}>
-          <CloudSvg key={i} className={styles.init} />
-        </animated.div>
+      {springs.map((spring, i) => (
+        <CloudSvg
+          key={i}
+          listRef={(instance: SVGElement | null) => {
+            if (!instance || initialClouds.current.includes(instance))
+              return;
+            initialClouds.current.push(instance);
+          }}
+          className={styles.init}
+          style={spring}
+        />
       ))}
-      {Array(24)
+      {Array(16)
         .fill(null)
         .map((v, i) => (
           <CloudSvg key={i} className={styles.off_page} />
