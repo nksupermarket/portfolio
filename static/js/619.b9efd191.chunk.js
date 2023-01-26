4b62d0b9dedfddec7de64704dd4fe8c95dbb11e1
alex <alexlhliang@gmail.com>
2023-01-26 11:04:10 -0800

    Updates

--- static/js/619.b9efd191.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[619],{619:function(s,a,r){r.r(a),r.d(a,{default:function(){return _}});r(791);var t="StarsBg_main__Uvlds",e="StarsBg_stars__T9gLN",i="StarsBg_stars2__+yJlI",l="StarsBg_stars3__-ALgE",n=r(184);function _(){return(0,n.jsxs)("div",{className:t,children:[(0,n.jsx)("div",{className:e}),(0,n.jsx)("div",{className:i}),(0,n.jsx)("div",{className:l})]})}}}]);
-//# sourceMappingURL=619.b9efd191.chunk.js.map
